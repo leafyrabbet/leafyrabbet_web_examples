2ca5fef1f7c37e3e87eeedc83fc713464b80d39e
Tommy Vegetables <leafyrabbet@gmail.com>
2020-02-23 19:32:21 -0500

    Backbone.js: In-Progess Update to Example

--- howto_backbone-js/index.js
+++ howto_backbone-js/index.js
@@ -8,16 +8,37 @@ Copyright (c) TenonGarden Productions, 2020
 
 // Assumes `backbone.js` has been loaded
 
-let Books = Backbone.Collection.extend(
+
+let BodyView = Backbone.View.extend({
+  el: 'body'
+});
+
+let BookModelSet = Backbone.Collection.extend(
   {
     url: '/books',
     parse: function(data) { return (data.books); }
   }
 );
 
-_.extend(Books, Backbone.Events);
+_.extend(BookModelSet, Backbone.Events);
 
-Books.on(
+BookModelSet.on(
   "alert",
   function(msg) { alert("Triggered " + msg); }
 );
+
+let BookView = Backbone.View.extend({
+  tagName: "div",
+  className: "_book",
+  events: {
+    "click .icon":          "open",
+    "click .button.edit":   "openEditDialog",
+    "click .button.delete": "destroy"
+  },
+  initialize: function() {
+    this.listenTo(this.model, "change", this.render);
+  },
+  render: function() {
+    return (this);
+  }
+});
